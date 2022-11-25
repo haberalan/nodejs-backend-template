516ba86f86e73d92826ec3cfd8b0e89883c39cd6
Alan <alanhaberapp@pm.me>
2022-11-25 12:15:23 +0100

    added body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,7 +11,8 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 
 // Middlewares
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 // Request information
